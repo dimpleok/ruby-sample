145b91b132c3aee0436842c92514799d3748f7c7
Jiushi Wan <dimpleok@JiushimatoMacBook-Air.local>
2015-11-28 19:08:13 +0800

    other forms of loop

--- loop.rb
+++ loop.rb
@@ -4,3 +4,13 @@
 5.times {
     puts "hello,world"
 }
+
+for i in 0..5
+       puts "Value of local variable is #{i}"
+end
+
+puts "put it in another way"
+
+(0..5).each do |i|
+       puts "Value of local variable is #{i}"
+end
